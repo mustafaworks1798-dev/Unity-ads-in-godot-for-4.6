9490c3eab044bd0ec7583286d461463cd35ee241
mustafaworks1798-dev <mustafaworks1798@gmail.com>
2026-05-14 17:53:18 +0300

    Update UnityAdsPlugin.kt

--- android/src/main/kotlin/com/replit/unityadsplugin/UnityAdsPlugin.kt
+++ android/src/main/kotlin/com/replit/unityadsplugin/UnityAdsPlugin.kt
@@ -26,78 +26,40 @@ class UnityAdsPlugin(godot: Godot) : GodotPlugin(godot),
             SignalInfo("ad_showed", String::class.java),
             SignalInfo("ad_failed_to_show", String::class.java, String::class.java),
             SignalInfo("ad_clicked", String::class.java),
-            SignalInfo("ad_closed", String::class.java),
+            SignalInfo("ad_closed", String::class.java)
         )
     }
 
     @UsedByGodot
     fun initialize(gameId: String, testMode: Boolean) {
-        runOnUiThread {
-            UnityAds.initialize(activity!!, gameId, testMode, this)
-        }
+        runOnUiThread { UnityAds.initialize(activity!!, gameId, testMode, this) }
     }
 
     @UsedByGodot
-    fun load_ad(placementId: String) {
-        UnityAds.load(placementId, this)
-    }
+    fun load_ad(placementId: String) { UnityAds.load(placementId, this) }
 
     @UsedByGodot
     fun show_ad(placementId: String) {
-        runOnUiThread {
-            UnityAds.show(activity!!, placementId, UnityAdsShowOptions(), this)
-        }
+        runOnUiThread { UnityAds.show(activity!!, placementId, UnityAdsShowOptions(), this) }
     }
 
     @UsedByGodot
-    fun is_initialized(): Boolean = UnityAds.isInitialized()
+    fun sdk_initialized(): Boolean = UnityAds.isInitialized()
 
-    // --- Initialization Callbacks ---
-    override fun onInitializationComplete() {
-        emitSignal("initialization_complete")
-    }
-
-    override fun onInitializationFailed(
-        error: UnityAds.UnityAdsInitializationError,
-        message: String
-    ) {
+    override fun onInitializationComplete() { emitSignal("initialization_complete") }
+    override fun onInitializationFailed(error: UnityAds.UnityAdsInitializationError, message: String) {
         emitSignal("initialization_failed", message)
     }
-
-    // --- Load Callbacks ---
-    override fun onUnityAdsAdLoaded(placementId: String) {
-        emitSignal("ad_loaded", placementId)
-    }
-
-    override fun onUnityAdsFailedToLoad(
-        placementId: String,
-        error: UnityAds.UnityAdsLoadError,
-        message: String
-    ) {
+    override fun onUnityAdsAdLoaded(placementId: String) { emitSignal("ad_loaded", placementId) }
+    override fun onUnityAdsFailedToLoad(placementId: String, error: UnityAds.UnityAdsLoadError, message: String) {
         emitSignal("ad_failed_to_load", placementId, message)
     }
-
-    // --- Show Callbacks ---
-    override fun onUnityAdsShowComplete(
-        placementId: String,
-        state: UnityAds.UnityAdsShowCompletionState
-    ) {
+    override fun onUnityAdsShowComplete(placementId: String, state: UnityAds.UnityAdsShowCompletionState) {
         emitSignal("ad_closed", placementId)
     }
-
-    override fun onUnityAdsShowFailure(
-        placementId: String,
-        error: UnityAds.UnityAdsShowError,
-        message: String
-    ) {
+    override fun onUnityAdsShowFailure(placementId: String, error: UnityAds.UnityAdsShowError, message: String) {
         emitSignal("ad_failed_to_show", placementId, message)
     }
-
-    override fun onUnityAdsShowStart(placementId: String) {
-        emitSignal("ad_showed", placementId)
-    }
-
-    override fun onUnityAdsShowClick(placementId: String) {
-        emitSignal("ad_clicked", placementId)
-    }
+    override fun onUnityAdsShowStart(placementId: String) { emitSignal("ad_showed", placementId) }
+    override fun onUnityAdsShowClick(placementId: String) { emitSignal("ad_clicked", placementId) }
 }
